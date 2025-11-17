78f0b747dde2ad35ef5954839ba098929b78550a
Romeo Fredson <19764867+SyllabusRomeo@users.noreply.github.com>
2025-11-17 14:59:40 +0000

    Make documentation button more visible in navigation

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -63,8 +63,9 @@ export default function Navigation({ onNavigate, currentPage, userEmail }: Navig
                   ? 'bg-blue-100 dark:bg-blue-900 text-blue-700 dark:text-blue-300 font-semibold'
                   : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700'
               }`}
+              title="Documentation"
             >
-              📚 Docs
+              📚 Documentation
             </button>
           </div>
 
